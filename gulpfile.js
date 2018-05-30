3265761d7dd9b9a848b5961afe4926f4bf476772
Nicholas Wease <skynet60@outlook.com>
2018-05-30 13:09:48 +0200

    added plugins into gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -22,6 +22,19 @@ gulp.task('css-main', function(){
         .pipe(gulp.dest('./public/dist/css/'))
 })
 
+gulp.task('css-home', function(){
+    return gulp.src(
+            [
+                './public/plugins/datatables/dataTables.bootstrap4.min.css',
+                './public/plugins/datatables/responsive.bootstrap4.min.css'
+            ]
+        )
+        .pipe(minifyCSS())
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+        .pipe(gp_concat('home.min.css'))
+        .pipe(gulp.dest('./public/dist/pages/home/css/'))
+})
+
 gulp.task('copy-fonts', function(){
     return gulp.src(
             ['./public/fonts/**']
@@ -29,7 +42,7 @@ gulp.task('copy-fonts', function(){
         .pipe(gulp.dest('./public/dist/fonts/'))
 })
 
-gulp.task('style', ['css-main', 'copy-fonts'], function(){})
+gulp.task('style', ['css-main', 'copy-fonts', 'css-home'], function(){})
 
 gulp.task('js-vendor', function(){
     return gulp.src(
@@ -86,7 +99,22 @@ gulp.task('js-page-dashboard', function(){
         .pipe(gulp.dest('./public/dist/pages/dashboard/js/'))
 });
 
-gulp.task('js', ['js-vendor', 'js-app', 'js-page-dashboard'], function(){})
+gulp.task('js-page-home', function(){
+    return gulp.src(
+            [
+                './public/plugins/datatables/jquery.dataTables.min.js',
+                './public/plugins/datatables/dataTables.bootstrap4.min.js',
+                './public/plugins/datatables/dataTables.responsive.min.js'
+            ]
+        )
+        .pipe(gp_concat('home.min.js'))
+        .pipe(gulp.dest('./public/dist/js/'))
+        .pipe(gp_rename('home.min.js'))
+        .pipe(gp_uglify())
+        .pipe(gulp.dest('./public/dist/pages/home/js/'))
+});
+
+gulp.task('js', ['js-vendor', 'js-app', 'js-page-dashboard', 'js-page-home'], function(){})
 
 gulp.task('prod', ['style', 'js'], function(){})
 gulp.task('default', ['style', 'js'], function(){})
