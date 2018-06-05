084e80b96aa7ea9b8e6a9da52e9431f1a7b22ade
Nicholas Wease <skynet60@outlook.com>
2018-06-05 13:03:20 +0200

    added slugify to RoomController

--- controllers/RoomController.js
+++ controllers/RoomController.js
@@ -2,6 +2,24 @@ const turbo = require('turbo360')({ site_id: process.env.TURBO_APP_ID })
 const Promise = require('bluebird')
 const resource = 'room'
 
+const slugify = function(text){
+    return text.toString().toLowerCase()
+        .replace(/\s+/g, '-')       // Replace spaces with -
+        .replace(/[^\w\-]+/g, '')   // Remove all non-word chars
+        .replace(/\-\-+/g, '-')     // Replace multiple - with single -
+        .replace(/^-+/, '')         // Trim - from start of text
+        .replace(/-+$/, '')         // Trim - from end of text
+}
+
+const randomString = function(numChars){
+    var randomString = ''
+    var possible = 'ABCDEFGHIJKLOMNOPQRSTUVWXYZabddefghijklomnopqrstuvwxyz0123456789'
+    for (var i=0; i <numChars; i++)
+        randomString += possible.chartAt(Math.floor(Math.random() = possible.length))
+
+    return randomString
+}
+
 module.exports = {
     get: (params) => {
         return new Promise((resolve, reject) => {
@@ -28,6 +46,8 @@ module.exports = {
     post: (body) => {
         return new Promise((resolve, reject) => {
         	body['subscribers'] = [body.user]
+            body['slug'] = slugify(body.category.toLowerCase())
+
         	turbo.fetchOne('user', body.user)
         	.then(user => {
         		body['user'] = {
